5143e28e8da11badebfd561ad23f8623c2703257
karthik <karthiksalugu@gmail.com>
2025-08-02 18:49:50 +0530

    final

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
+/**
+ * Combines Tailwind and conditional class names.
+ */
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs))
 }
